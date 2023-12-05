e96dbd00591f461a30f31e1463cabcd9ad736986
meodai <meodai@gmail.com>
2023-12-05 20:58:15 +0000

    Deploying to gh-pages from @ meodai/rampensau@cf0140f5542fad66a299ee8e742326cd299e4bc2 🚀

--- index.js
+++ index.js
@@ -25,17 +25,16 @@ var rampensau = (() => {
     lRange = [Math.random() * 0.1, 0.9],
     lEasing = (x) => Math.pow(x, 1.5)
   } = {}) {
-    const hueSlice = 360 / total;
-    const hues = new Array(total).fill(0).map((_, i) => (360 + (hStart + i * hCycles * hueSlice)) % 360);
     const lDiff = lRange[1] - lRange[0];
     const sDiff = sRange[1] - sRange[0];
-    const firstColor = [hues.pop() || 0, sRange[0], lRange[0]];
-    const ramp = new Array(total - 1).fill(0).map((_, i) => [
-      hues.pop() || 0,
-      sRange[0] + sDiff * sEasing((i + 1) / (total - 1)),
-      lRange[0] + lDiff * lEasing((i + 1) / (total - 1))
-    ]);
-    return [...[firstColor], ...ramp];
+    return new Array(total).fill(0).map((_, i) => {
+      const relI = i / (total - 1);
+      return [
+        (360 + hStart + (1 - relI) * (360 * hCycles)) % 360,
+        sRange[0] + sDiff * sEasing(relI),
+        lRange[0] + lDiff * lEasing(relI)
+      ];
+    });
   }
   function map(n, start1, stop1, start2, stop2) {
     return (n - start1) / (stop1 - start1) * (stop2 - start2) + start2;
